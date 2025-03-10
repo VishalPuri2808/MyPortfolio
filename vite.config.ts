d309c22af48a0f9ef163981901ebc1f79e78e4e9
Vishal <vishalpuri2800@gmail.com>
2025-03-10 00:02:43 -0500

    vite updated for github host

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://vishalpuri2808.github.io/MyPortfolio/', // Set this to your GitHub repository name
+  base: '/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
