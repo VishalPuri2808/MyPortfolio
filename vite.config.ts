3889c682324a99df6700868e5aa59b1d40866765
Vishal <vishalpuri2800@gmail.com>
2025-03-15 05:42:36 -0500

    Fixed deployment issues2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MyPortfolio/", // Ensure it matches your repository name exactly
   plugins: [react()],
   build: {
     outDir: "dist",
