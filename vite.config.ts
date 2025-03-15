95af706bd004f907f0304a30b21f1fa4268da7a7
Vishal <vishalpuri2800@gmail.com>
2025-03-15 03:24:50 -0500

    github

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: "/MyPortfolio/", // Ensure it matches your repository name exactly
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    open: true,
   },
 });
